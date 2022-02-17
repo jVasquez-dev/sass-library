50a7461830f6628183c4f12b8503ac341f463ac9
Jorge Vasquez <jvasquezn04@gmail.com>
2022-02-17 15:57:40 -0600

    library version 1.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 const { src, dest, watch, series } = require('gulp')
-const sass = require('gulp-sass')(require('sass'));
+const sass = require('gulp-sass')(require('sass'))
+const purgecss = require('gulp-purgecss')
 
 function buildStyles() {
-  return src('index.scss')
-    .pipe(sass())
+  return src('sass/**/*.scss')
+    .pipe(sass(sass({ outputStyle: 'compressed' })))
+    .pipe(purgecss({ content: ['*.html'] }))
     .pipe(dest('css'))
 }
 
 function watchTask() {
-  watch(['index.scss'], buildStyles)
+  watch(['sass/**/*.scss'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
